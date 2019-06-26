6be100a7dead88c50ee8c4998a23b7735bc7c5c4
LeeHyeGyu <gprb4740@naver.com>
2019-06-26 14:40:36 +0900

    Add files via upload

---
+++ ONESHOT/src/main/webapp/resources/js/filter.js
@@ -0,0 +1,24 @@
+var accordion = function(){
+  var data = $('.accordion').attr('data-accordion')
+  
+  $('.accordion-header').on('click', function(){
+ if (data === 'close'){
+  alert();
+   $('.accordion-body').slideUp()
+   if ($(this).hasClass('active')){
+     $(this).toggleClass('active')
+     }else{
+    $('.accordion-header').removeClass('active')
+     $(this).toggleClass('active')
+   }
+ } 
+ else {
+   $(this).toggleClass('active')
+ }
+   
+
+    $(this).next('.accordion-body').not(':animated').slideToggle()
+  })
+}
+
+accordion();
